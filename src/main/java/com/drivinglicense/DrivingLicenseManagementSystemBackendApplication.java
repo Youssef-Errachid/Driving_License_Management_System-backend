cef3792256959261edd9c8ab95ec0b947a9ab9e4
Youssef <yousseferrachid@gmail.com>
2026-08-25 15:20:42 +0100

    initialize Spring Boot project with core dependencies

---
+++ src/main/java/com/drivinglicense/DrivingLicenseManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.drivinglicense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingLicenseManagementSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrivingLicenseManagementSystemBackendApplication.class, args);
+    }
+
+}
