c5e3af60ec68a7649511af494c781ce12ef38e5a
Youssef <yousseferrachid@gmail.com>
2026-09-08 10:10:25 +0100

    add flyway seed migrations for exam_type_config and license_category

---
+++ src/main/resources/db/migration/V2__seed_exam_type_config.sql
@@ -0,0 +1,3 @@
+INSERT INTO exam_type_config (exam_type, fee) VALUES
+                                                  ('VISION', 10.00),
+                                                  ('THEORY', 20.00);
---
+++ src/main/resources/db/migration/V3__seed_license_category.sql
@@ -0,0 +1,28 @@
+INSERT INTO license_category (name, description, minimum_age, validation_duration_years, fee) VALUES
+                                                                                                  ('Catégorie 1 - Moto légère',
+                                                                                                   'Permet de conduire les motos de petite cylindrée et de puissance limitée.',
+                                                                                                   18, 5, 15.00),
+
+                                                                                                  ('Catégorie 2 - Moto lourde',
+                                                                                                   'Permet de conduire les motos de grande cylindrée et de forte puissance.',
+                                                                                                   21, 5, 30.00),
+
+                                                                                                  ('Catégorie 3 - Véhicule léger',
+                                                                                                   'Permet de conduire les véhicules légers et voitures particulières.',
+                                                                                                   18, 10, 20.00),
+
+                                                                                                  ('Catégorie 4 - Transport commercial (Taxi/Limousine)',
+                                                                                                   'Permet de conduire les taxis et limousines.',
+                                                                                                   21, 10, 200.00),
+
+                                                                                                  ('Catégorie 5 - Véhicule agricole',
+                                                                                                   'Permet de conduire tous les véhicules agricoles (tracteurs, engins de labour).',
+                                                                                                   21, 10, 50.00),
+
+                                                                                                  ('Catégorie 6 - Autobus petit et moyen',
+                                                                                                   'Permet de conduire les autobus de petite et moyenne capacité.',
+                                                                                                   21, 10, 250.00),
+
+                                                                                                  ('Catégorie 7 - Poids lourd',
+                                                                                                   'Permet de conduire les camions et véhicules lourds (autobus et camions de grande capacité).',
+                                                                                                   21, 10, 300.00);
