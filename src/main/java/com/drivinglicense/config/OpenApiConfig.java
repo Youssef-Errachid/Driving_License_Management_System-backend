1265061df5ad8296ded22f2af5038fb23cc96f8c
Youssef <yousseferrachid@gmail.com>
2026-09-09 16:56:00 +0100

    add Swagger/OpenAPI configuration

---
+++ src/main/java/com/drivinglicense/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.drivinglicense.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Driving License Management System", version = "V1"),
+        security = @SecurityRequirement(name = "bearerAuth"),
+        tags = {
+                @Tag(name = "Authentication", description = "User login and registration")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
+
